8cb16e9f51a80e89ee7f1bdd7b36d93a9672f9a2
David Hollinger <66143861+dholling4@users.noreply.github.com>
2023-12-30 16:29:47 +0000

    added the Digital Athlete pages

---
+++ pages/TheraTrack.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.markdown("# Welcome to TheraTrack 👩‍⚕️")
+st.markdown("### Empowering Patients Together\U0001F4AA")
+st.sidebar.markdown("# TheraTrack 👩‍⚕️")
+
+st.button("My patients :closed_lock_with_key:")
+
+st.write("#### Coming Soon...")
