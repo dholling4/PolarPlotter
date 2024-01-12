5b34b319b37845496c22c7f71e8fc84e1666324b
David Hollinger <66143861+dholling4@users.noreply.github.com>
2024-01-12 21:58:31 +0000

    about page images

--- pages/About.py
+++ pages/About.py
@@ -19,8 +19,8 @@ st.sidebar.markdown("# About")
 #     st.write(f"**Description:** {description}")
     
 # List of person data (replace with your data)
-path = "/workspaces/PolarPlotter/headshots/"
-path = "https://github.com/dholling4/PolarPlotter/blob/main/headshots/"
+path = "https://raw.githubusercontent.com/dholling4/PolarPlotter/main/headshots/"
+
 persons = [
     {"image_url": path + "allison.png", "name": "Allison Tanner", "description": "MBA student, Team Captain of Auburn Track & Field"},
     {"image_url": path + "david_e.png", "name": "David Edmonson", "description": "Master student, Mechanical Engineering, Auburn Decathlete"},
