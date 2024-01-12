2069ed328bb98202fa162f118121e4da6dd771ba
David Hollinger <66143861+dholling4@users.noreply.github.com>
2024-01-12 22:11:52 +0000

    test

--- pages/TheFemaleAthlete.py
+++ pages/TheFemaleAthlete.py
@@ -6,5 +6,7 @@ st.sidebar.markdown("# The Female Athlete :woman-lifting-weights:")
 
 persons = [
     {"image_url": "https://raw.githubusercontent.com/dholling4/PolarPlotter/main/the_female_athlete.png", "name": "Coming Soon...", "description": " "},
+    {"image_url": "https://raw.githubusercontent.com/dholling4/PolarPlotter/main/baseline_pics/depth_squat_enhanced.png", "name": "Coming Soon...", "description": " "},
+
 ]  
-st.image(persons[0]["image_url"], caption=f"{persons[0]['name']}", width=500)
+st.image(persons[1]["image_url"], caption=f"{persons[1]['name']}", width=500)
