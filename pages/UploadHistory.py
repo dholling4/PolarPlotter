cb29aa9a907ac097bc7117d98bf2dc8c4c869b58
David Hollinger <66143861+dholling4@users.noreply.github.com>
2024-01-13 04:36:57 +0000

    comment out UploadHistory

--- pages/UploadHistory.py
+++ pages/UploadHistory.py
@@ -1,71 +1,71 @@
-import streamlit as st
-from hugchat import hugchat
-from hugchat.login import Login
-
-# # App title
-# st.set_page_config(page_title="AI Coach 🦾 🤖")
-
-# st.write("""### 🦾 🤖 Introducing Whalley, Your Personal AI Coach for Peak Performance! 
-         
-# 🌟 Welcome to the future of athletic training with Whalley, your dedicated AI coach on the Digital Athlete app! 🌟""")
-# whalley_expander = st.expander("📖 Learn more about Whalley", expanded=False)         
-
-# with whalley_expander:
-#     st.markdown("""Whalley is a state-of-the-art AI coach that uses the latest in natural language processing to provide you with personalized feedback and advice to help you reach your peak performance! 🏃‍♀️ 🏃‍♂️""")
-             
-# # ✨ **Data-Driven Insights**: Whalley utilizes state-of-the-art algorithms to analyze data from your wearables, providing precise insights into your biomechanics and performance metrics.
-
-# # 🤖 **Adaptive Training**: No two athletes are the same. Whalley tailors your training regimen based on your unique movement signature, ensuring that every workout is optimized for YOUR success.
-
-# # 👟 **Smart Gear Recommendations**: Say goodbye to guesswork! Whalley recommends the best footwear and equipment based on your biomechanics, enhancing comfort, reducing injury risks, and optimizing performance.
-
-# # 🔒 **Progress Tracking**: Track your journey effortlessly with Whalley. Monitor your improvements, set new goals, and celebrate achievements with our user-friendly interface.
-
-# # 👥 **Include Expert Opinion**: Easily share your progress with coaches, trainers, and physical therapists, fostering collaboration for even better results.
-
-# # 💡 **Continuous Learning**: Whalley evolves with you. As you grow, so does your AI coach. Benefit from continuous learning algorithms that adapt to your changing needs.
-
-# # 🌐 **The Digital Athlete Community**: Join a thriving community of like-minded individuals. Share experiences, participate in challenges, and motivate each other towards greatness!
-
-# # 📱 **Seamless Integration**: The Digital Athlete app seamlessly integrates with your wearables, making it easy for you to focus on your training while Whalley takes care of the rest.
-
-# # 🚀 **Unleash Your Potential**: Whether you're a seasoned athlete or just starting, Whalley empowers you to reach new heights. If you can move, Whalley can measure it!
-
-# # Ready to embark on your journey to peak performance? Download the Digital Athlete app and let Whalley be your guide to success! 🚀💪""")
-
-# Hugging Face Credentials
-with st.sidebar:
-    st.title('AI Coach 🦾 🤖')
-    # if ('EMAIL' in st.secrets) and ('PASS' in st.secrets):
-    #     st.success('HuggingFace Login credentials already provided!', icon='✅')
-    #     hf_email = st.secrets['EMAIL']
-    #     hf_pass = st.secrets['PASS']
-    # else:
-    #     hf_email = st.text_input('Enter E-mail:', type='password')
-    #     hf_pass = st.text_input('Enter password:', type='password')
-    #     if not (hf_email and hf_pass):
-    #         st.warning('Please enter your credentials!', icon='⚠️')
-    #     else:
-    #         st.success('Proceed to entering your prompt message!', icon='👉')
-    # st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-an-llm-powered-chatbot-with-streamlit/)!')
-
-# ## OPEN AI
-    
 # import streamlit as st
-# from langchain.llms import OpenAI
+# from hugchat import hugchat
+# from hugchat.login import Login
 
-# st.title('🦜🔗 Quickstart App')
+# # # App title
+# # st.set_page_config(page_title="AI Coach 🦾 🤖")
 
-# openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+# # st.write("""### 🦾 🤖 Introducing Whalley, Your Personal AI Coach for Peak Performance! 
+         
+# # 🌟 Welcome to the future of athletic training with Whalley, your dedicated AI coach on the Digital Athlete app! 🌟""")
+# # whalley_expander = st.expander("📖 Learn more about Whalley", expanded=False)         
 
-# def generate_response(input_text):
-#     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-#     st.info(llm(input_text))
+# # with whalley_expander:
+# #     st.markdown("""Whalley is a state-of-the-art AI coach that uses the latest in natural language processing to provide you with personalized feedback and advice to help you reach your peak performance! 🏃‍♀️ 🏃‍♂️""")
+             
+# # # ✨ **Data-Driven Insights**: Whalley utilizes state-of-the-art algorithms to analyze data from your wearables, providing precise insights into your biomechanics and performance metrics.
 
-# with st.form('my_form'):
-#     text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
-#     submitted = st.form_submit_button('Submit')
-#     if not openai_api_key.startswith('sk-'):
-#         st.warning('Please enter your OpenAI API key!', icon='⚠')
-#     if submitted and openai_api_key.startswith('sk-'):
-#         generate_response(text)
+# # # 🤖 **Adaptive Training**: No two athletes are the same. Whalley tailors your training regimen based on your unique movement signature, ensuring that every workout is optimized for YOUR success.
+
+# # # 👟 **Smart Gear Recommendations**: Say goodbye to guesswork! Whalley recommends the best footwear and equipment based on your biomechanics, enhancing comfort, reducing injury risks, and optimizing performance.
+
+# # # 🔒 **Progress Tracking**: Track your journey effortlessly with Whalley. Monitor your improvements, set new goals, and celebrate achievements with our user-friendly interface.
+
+# # # 👥 **Include Expert Opinion**: Easily share your progress with coaches, trainers, and physical therapists, fostering collaboration for even better results.
+
+# # # 💡 **Continuous Learning**: Whalley evolves with you. As you grow, so does your AI coach. Benefit from continuous learning algorithms that adapt to your changing needs.
+
+# # # 🌐 **The Digital Athlete Community**: Join a thriving community of like-minded individuals. Share experiences, participate in challenges, and motivate each other towards greatness!
+
+# # # 📱 **Seamless Integration**: The Digital Athlete app seamlessly integrates with your wearables, making it easy for you to focus on your training while Whalley takes care of the rest.
+
+# # # 🚀 **Unleash Your Potential**: Whether you're a seasoned athlete or just starting, Whalley empowers you to reach new heights. If you can move, Whalley can measure it!
+
+# # # Ready to embark on your journey to peak performance? Download the Digital Athlete app and let Whalley be your guide to success! 🚀💪""")
+
+# # Hugging Face Credentials
+# with st.sidebar:
+#     st.title('AI Coach 🦾 🤖')
+#     # if ('EMAIL' in st.secrets) and ('PASS' in st.secrets):
+#     #     st.success('HuggingFace Login credentials already provided!', icon='✅')
+#     #     hf_email = st.secrets['EMAIL']
+#     #     hf_pass = st.secrets['PASS']
+#     # else:
+#     #     hf_email = st.text_input('Enter E-mail:', type='password')
+#     #     hf_pass = st.text_input('Enter password:', type='password')
+#     #     if not (hf_email and hf_pass):
+#     #         st.warning('Please enter your credentials!', icon='⚠️')
+#     #     else:
+#     #         st.success('Proceed to entering your prompt message!', icon='👉')
+#     # st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-an-llm-powered-chatbot-with-streamlit/)!')
+
+# # ## OPEN AI
+    
+# # import streamlit as st
+# # from langchain.llms import OpenAI
+
+# # st.title('🦜🔗 Quickstart App')
+
+# # openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+
+# # def generate_response(input_text):
+# #     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+# #     st.info(llm(input_text))
+
+# # with st.form('my_form'):
+# #     text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+# #     submitted = st.form_submit_button('Submit')
+# #     if not openai_api_key.startswith('sk-'):
+# #         st.warning('Please enter your OpenAI API key!', icon='⚠')
+# #     if submitted and openai_api_key.startswith('sk-'):
+# #         generate_response(text)
