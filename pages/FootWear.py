777fbc6484948743a6ff51f3de31ff02cdc8a1d6
David Hollinger <66143861+dholling4@users.noreply.github.com>
2024-02-01 01:10:51 +0000

    footWear

---
+++ pages/FootWear.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.markdown("# Welcome to FootWear 👨‍🏫")
+st.markdown("### FootWear: Smart Strides, Better Rides.\U0001F45F")
+st.sidebar.markdown("# FootWear \U0001F45F")
+
+"""
+Step into the future of athletic performance with our revolutionary 'FootWear' feature in The Digital Athlete app. More than just a play on words, FootWear brings cutting-edge computer vision technology to your fingertips, offering personalized insights into the condition of your beloved athletic shoes.
+
+Simply snap a picture of the sole of your shoe, and let our advanced algorithms do the rest. FootWear doesn't just tell you when it's time to replace your shoes; it's your virtual footwear consultant. The feature intelligently analyzes the wear and tear patterns, providing recommendations on the type of shoes that best suit your unique gait and activity level.
+
+But that's not all – FootWear goes beyond the surface. Dive into the details of your biomechanics as the algorithm classifies your pronation style. Whether you overpronate, supinate, or have a normal pronation pattern, FootWear tailors its suggestions to optimize your comfort, performance, and overall foot health.
+
+Say goodbye to guesswork and hello to a personalized shoe experience. Step confidently, step smartly – with FootWear, every stride is a step towards peak performance and comfort.
+"""
