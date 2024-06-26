f690b691fdf5213636a426f7c2a1ff50258b41bd
David Hollinger <66143861+dholling4@users.noreply.github.com>
2024-06-26 03:06:07 +0000

    update AI coach to coming soon...

--- pages/AICoach.py
+++ pages/AICoach.py
@@ -1,37 +1,38 @@
-from openai import OpenAI
+# from openai import OpenAI
 import streamlit as st
-import os
-import sys
-from dotenv import load_dotenv, dotenv_values
-load_dotenv()
-# 
-import openai
+# import os
+# import sys
+# from dotenv import load_dotenv, dotenv_values
+# load_dotenv()
+# # 
+# import openai
 
-openai.api_key = "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"
-#
-import os
+# openai.api_key = "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"
+# #
+# import os
 
-os.environ["OPENAI_API_KEY"] = "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"
-# initialize the client
-client = OpenAI(
-  base_url="https://api-inference.huggingface.co/v1",
-  api_key=os.environ.get('HUGGINGFACEHUB_API_TOKEN')  #"hf_xxx" # Replace with your token
-) 
+# os.environ["OPENAI_API_KEY"] = "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"
+# # initialize the client
+# client = OpenAI(
+#   base_url="https://api-inference.huggingface.co/v1",
+#   api_key=os.environ.get('HUGGINGFACEHUB_API_TOKEN')  #"hf_xxx" # Replace with your token
+# ) 
 
 
 st.title("🤖 Whalley: Your AI Virtual Coach")
 
-import streamlit as st 
-from langchain.llms import Ollama
-llm = Ollama(model="llama2-uncensored:latest") # 👈 stef default
+st.text("Coming Soon...")
+# import streamlit as st 
+# from langchain.llms import Ollama
+# llm = Ollama(model="llama2-uncensored:latest") # 👈 stef default
 
-colA, colB = st.columns([.90, .10])
-with colA:
-    prompt = st.text_input("prompt", value="", key="prompt")
-response = ""
-with colB:
-    st.markdown("")
-    st.markdown("")
-    if st.button("🙋‍♀️", key="button"):
-        response = llm.predict(prompt)
-st.write(response)
+# colA, colB = st.columns([.90, .10])
+# with colA:
+#     prompt = st.text_input("prompt", value="", key="prompt")
+# response = ""
+# with colB:
+#     st.markdown("")
+#     st.markdown("")
+#     if st.button("🙋‍♀️", key="button"):
+#         response = llm.predict(prompt)
+# st.write(response)
