74f111650ef5e17a486148da5480c89dcd11d0ae
David Hollinger <66143861+dholling4@users.noreply.github.com>
2025-01-19 16:44:00 +0000

    pip install imageio[ffmpeg] numpy

--- pages/gait.py
+++ pages/gait.py
@@ -3,13 +3,13 @@ import cv2
 import mediapipe as mp
 import tempfile
 import os
-
+import moviepy.editor as m
 # Setup MediaPipe Pose model
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 mp_drawing = mp.solutions.drawing_utils
 
-import moviepy.editor at mp
+# import moviepy.editor at mp
         
 def convert_mov_to_mp4(input_path, output_path):
     clip = mp.VideoFileClip(input_path)
