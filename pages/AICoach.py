f029b08617a94f4095cea6f6be7d645dfc92cb32
David Hollinger <66143861+dholling4@users.noreply.github.com>
2025-03-02 15:27:20 +0000

    openai

--- pages/AICoach.py
+++ pages/AICoach.py
@@ -1,5 +1,45 @@
 import streamlit as st
 st.write("coming soon...")
+
+from openai import OpenAI
+
+YOUR_API_KEY = "INSERT API KEY HERE"
+
+messages = [
+    {
+        "role": "system",
+        "content": (
+            "You are an artificial intelligence assistant and you need to "
+            "engage in a helpful, detailed, polite conversation with a user."
+        ),
+    },
+    {
+        "role": "user",
+        "content": (
+            "Count to 100, with a comma between each number and no newlines. "
+            "E.g., 1, 2, 3, ..."
+        ),
+    },
+]
+
+client = OpenAI(api_key=YOUR_API_KEY, base_url="https://api.perplexity.ai")
+
+# demo chat completion without streaming
+response = client.chat.completions.create(
+    model="mistral-7b-instruct",
+    messages=messages,
+)
+print(response)
+
+# demo chat completion with streaming
+response_stream = client.chat.completions.create(
+    model="mistral-7b-instruct",
+    messages=messages,
+    stream=True,
+)
+for response in response_stream:
+    print(response)
+    
 # # from openai import OpenAI
 # import streamlit as st
 # # import os
