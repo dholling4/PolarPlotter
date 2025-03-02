99ed85042a2b6821903b0542abddcc322d95405d
David Hollinger <66143861+dholling4@users.noreply.github.com>
2025-03-02 15:32:39 +0000

    api never works!!!

--- pages/AICoach.py
+++ pages/AICoach.py
@@ -1,45 +1,6 @@
 import streamlit as st
 st.write("coming soon...")
 
-from openai import OpenAI
-
-YOUR_API_KEY = "xai-FS1pD8kWKBbCz68Q02CuzYIykPaIzlyAr0S1rvDnAWG6lk8EDqO6Pl4A9FyFaJ8eS0GhHSdHbaPwZXlz"
-
-messages = [
-    {
-        "role": "system",
-        "content": (
-            "You are an artificial intelligence assistant and you need to "
-            "engage in a helpful, detailed, polite conversation with a user."
-        ),
-    },
-    {
-        "role": "user",
-        "content": (
-            "Count to 100, with a comma between each number and no newlines. "
-            "E.g., 1, 2, 3, ..."
-        ),
-    },
-]
-
-client = OpenAI(api_key=YOUR_API_KEY, base_url="https://api.perplexity.ai")
-
-# demo chat completion without streaming
-response = client.chat.completions.create(
-    model="mistral-7b-instruct",
-    messages=messages,
-)
-print(response)
-
-# demo chat completion with streaming
-response_stream = client.chat.completions.create(
-    model="mistral-7b-instruct",
-    messages=messages,
-    stream=True,
-)
-for response in response_stream:
-    print(response)
-
 # # from openai import OpenAI
 # import streamlit as st
 # # import os
