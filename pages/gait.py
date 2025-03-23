a3bf0a72e58a106adf378e618b23b2831808eb5d
David Hollinger <66143861+dholling4@users.noreply.github.com>
2025-03-23 16:22:07 +0000

    update

--- pages/gait.py
+++ pages/gait.py
@@ -1801,14 +1801,12 @@ def send_email(to_email, attachment_path):
 
     load_dotenv()
 
-    # sender_email = EMAIL_ADDRESS
-    # app_password = EMAIL_APP_PASSWORD
     sender_email = os.getenv("EMAIL_ADDRESS")
     app_password = os.getenv("EMAIL_APP_PASSWORD")
 
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
         smtp.login(sender_email, app_password)
-        st.write("✅ Email sent!")
+        st.write("✅ Email sent!!!")
     
     msg = EmailMessage()
     msg['Subject'] = "Your Stride Sync Report"
