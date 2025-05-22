2490a3fe5f3ea54954ad9e149102d60cf0b416e1
David Hollinger <66143861+dholling4@users.noreply.github.com>
2025-05-22 14:53:31 +0000

    d

--- pages/gait.py
+++ pages/gait.py
@@ -478,6 +478,7 @@ def process_first_frame_report(video_path, video_index):
     
     ret, frame = cap.read()
     if rotated:
+        st.write("rotated!")
         frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
 
     if not ret:
